1b48e5e32a45fd933c67e5bf106df18d3208f6b9
grudengroup\zhen.yuan <zhen.yuan@gruden.com>
2018-12-04 11:56:37 +1100

    Create dynamice object.

---
+++ src/DynamicObject.UnitTests/code/DynamicObjectTests.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Xunit;
+
+namespace DynamicObjects.Tests
+{
+    public class DynamicObjectTests
+    {
+        [Fact]
+        public void XUnitTestDynamicObject()
+        {
+            var car = new DynamicObject();
+            // Create properties
+            car.Instance.Make = "G35";
+            car.Instance.Year = 2014;
+            car.Instance.CurrentSpeed = 45;
+
+            // Add property in a more dynamic manor.
+            car.AddProperty("TopSpeed", 180);
+
+            var output = JsonConvert.SerializeObject(car);
+
+            // {"Instance":{"Make":"G35","Year":2014,"CurrentSpeed":45,"TopSpeed":180}}
+            Assert.Equal("{\"Instance\":{\"Make\":\"G35\",\"Year\":2014,\"CurrentSpeed\":45,\"TopSpeed\":180}}", output);
+        }
+    }
+}
---
+++ src/DynamicObject/code/DynamicObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace DynamicObjects
+{
+    public class DynamicObject
+    {
+        public dynamic Instance = new ExpandoObject();
+
+        public void AddProperty(string name, object value)
+        {
+            ((IDictionary<string, object>)this.Instance).Add(name, value);
+        }
+
+        public dynamic GetProperty(string name)
+        {
+            if (((IDictionary<string, object>)this.Instance).ContainsKey(name))
+                return ((IDictionary<string, object>)this.Instance)[name];
+            else
+                return null;
+        }
+
+        public void AddMethod(Action methodBody, string methodName)
+        {
+            this.AddProperty(methodName, methodBody);
+        }
+    }
+
+}
